bc2eece0555bfce71fa3dfb4993ff6af1e189679
zawa-tin <contactaizawa@icloud.com>
2023-06-04 02:15:53 +0900

    verify EratosthenesSieve

--- Src/Number/EratosthenesSieve.hpp
+++ Src/Number/EratosthenesSieve.hpp
@@ -4,39 +4,45 @@
 
 #include <vector>
 #include <cassert>
+#include <algorithm>
 
 namespace zawa {
 
 class EratosthenesSieve {
 private:
-    usize size;
+    usize tableSize;
     std::vector<bool> table;
 
 public:
     EratosthenesSieve() = default;
 
-    EratosthenesSieve(usize size) : size{ size + 1 }, table(size + 1, true) {
-        assert(size > 0);
+    EratosthenesSieve(usize tableSize_) : tableSize{ tableSize_ + 1 }, table(tableSize_ + 1, true) {
+        assert(tableSize > 0);
         table[0] = table[1] = false;
-        for (usize i = 2 ; i < size ; i++) {
+        for (u64 i = 2 ; i * i < tableSize ; i++) {
             if (!table[i]) continue;
-            for (usize j = i + i ; j < size ; j += i ) {
+            for (u64 j = i * i ; j < tableSize ; j += i ) {
                 table[j] = false;
             }
         }
     }
 
-    inline bool operator[](const u32 i) const {
+    inline bool operator[](u32 i) const {
         return table[i];
     }
 
-    inline bool isPrime(const u32 i) const {
+    inline bool isPrime(u32 i) const {
         return table[i];
+    }
+
+    inline usize size() const {
+        return tableSize - 1;
     }
 
     std::vector<u32> enumeratePrimes(u32 N) const {
-        assert(N + 1 < size);
+        assert(N < tableSize);
         std::vector<u32> primes{};
+        primes.reserve(std::count(table.begin(), table.begin() + N + 1, true));
         for (u32 i = 2 ; i <= N ; i++) {
             if (table[i]) {
                 primes.emplace_back(i);
