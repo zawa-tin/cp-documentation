538134c3a5057a083fa4b56893f3ba50dc50dc38
zawa-tin <contactaizawa@icloud.com>
2023-09-30 01:51:22 +0900

    add Affine Monoid #116

--- Test/LC/point_set_range_composite.test.cpp
+++ Test/LC/point_set_range_composite.test.cpp
@@ -3,59 +3,34 @@
 #include "../../Src/Template/TypeAlias.hpp"
 #include "../../Src/Template/IOSetting.hpp"
 #include "../../Src/DataStructure/SegmentTree/SegmentTree.hpp"
-
-#include <iostream>
+#include "../../Src/Number/ModInt.hpp"
+#include "../../Src/Algebra/Monoid/AffineMonoid.hpp"
 
 using namespace zawa;
+using mint = StaticModInt<u32, 998244353>;
+using Monoid = AffineMonoid<mint>;
 
-constexpr u64 mod{ 998244353 };
-
-struct Linear {
-    u64 a{1}, b{};
-    constexpr Linear() {}
-    constexpr Linear(u64 a, u64 b) : a{a % mod}, b{b % mod} {}
-    u64 apply(u64 x) const {
-        return ((a * x) % mod + b) % mod;
-    }
-    friend std::ostream& operator<<(std::ostream& os, const Linear& v) {
-        os << '(' << v.a << ' ' << v.b << ')';
-        return os;
-    }    
-};
-
-struct Monoid {
-    using Element = Linear;
-    static constexpr Linear identity() {
-        return Linear{};
-    }
-    static constexpr Linear operation(const Linear& l, const Linear& r) {
-        return Linear{l.a * r.a, l.b * r.a + r.b};
-    }
-};
-
-#include <cassert>
+#include <iostream>
+#include <vector>
 
 int main() {
     SetFastIO();
     int n, q; std::cin >> n >> q;
-    std::vector<Linear> init(n);
+    std::vector<Affine<mint>> init(n);
     for (int i{} ; i < n ; i++) {
         u64 a, b; std::cin >> a >> b;
-        init[i] = Linear{ a, b };
+        init[i] = Affine<mint>{ a, b };
     }
     SegmentTree<Monoid> seg(init);
-    // seg.debug();
     for (int _{} ; _ < q ; _++) {
         int t; std::cin >> t;
         if (t == 0) {
-            int p; std::cin >> p;
-            u64 a, b; std::cin >> a >> b;
-            seg.set(p, Linear{ a, b });
+            int p, c, d; std::cin >> p >> c >> d;
+            seg.set(p, { c, d });
         }
         else if (t == 1) {
-            u32 l, r; std::cin >> l >> r;
-            u64 x; std::cin >> x;
-            std::cout << seg.product(l, r).apply(x) << std::endl;
+            int l, r, x; std::cin >> l >> r >> x;
+            std::cout << seg.product(l, r).transformation(x) << std::endl;
         }
         else {
             assert(false);
