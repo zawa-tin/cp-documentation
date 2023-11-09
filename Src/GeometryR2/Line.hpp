80ddcffd6e221ddd4edc4f34708a5e4b4be1af31
zawa-tin <contactaizawa@icloud.com>
2023-11-09 09:20:23 +0900

    implement Orh/Par between Lines

--- Src/GeometryR2/Line.hpp
+++ Src/GeometryR2/Line.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "./Point.hpp"
+#include "./Relation.hpp"
+
+#include <cassert>
 
 namespace zawa {
 
@@ -33,6 +36,22 @@ public:
     /* member function */
     bool isValid() const {
         return p0_ != p1_;
+    }
+
+    /* friend function */
+    friend bool Parallel(const Line& lhs, const Line& rhs) {
+        assert(lhs.isValid());
+        assert(rhs.isValid());
+        Vector lVector{lhs.p1() - lhs.p0()};
+        Vector rVector{rhs.p1() - rhs.p0()};
+        return Zero(Cross(lVector, rVector));
+    }
+    friend bool Orthgonal(const Line& lhs, const Line& rhs) {
+        assert(lhs.isValid());
+        assert(rhs.isValid());
+        Vector lVector{lhs.p1() - lhs.p0()};
+        Vector rVector{rhs.p1() - rhs.p0()};
+        return Zero(Dot(lVector, rVector));
     }
 };
 
---
+++ Test/AOJ/CGL_2_A.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_2_A"
+
+#include "../../Src/Template/IOSetting.hpp"
+#include "../../Src/GeometryR2/Line.hpp"
+
+int main() {
+    using namespace zawa;
+    using namespace geometryR2;
+    SetFastIO();
+    int q; std::cin >> q;
+    for (int _{} ; _ < q ; _++) {
+        Line l1, l2;
+        std::cin >> l1.p0() >> l1.p1() >> l2.p0() >> l2.p1();
+        if (Parallel(l1, l2)) std::cout << 2 << '\n';
+        else if (Orthgonal(l1, l2)) std::cout << 1 << '\n';
+        else std::cout << 0 << '\n';
+    }
+}
