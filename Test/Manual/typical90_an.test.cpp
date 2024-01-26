b46912ba8caa4837e3b197aa2be0e024a6401807
zawa-tin <contactaizawa@icloud.com>
2024-01-26 17:02:26 +0900

    Burn Bury #252

--- Test/Manual/typical90_an.test.cpp
+++ Test/Manual/typical90_an.test.cpp
@@ -1,41 +1,35 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
 
 #include "../../Src/Template/IOSetting.hpp"
-#include "../../Src/Graph/Flow/Dinic.hpp"
+#include "../../Src/Graph/Flow/BurnBury.hpp"
 
 #include <iostream>
 
 /*
  * 競プロ典型90問-040 Get More Money
- * https://atcoder.jp/contests/typical90/submissions/48902896
+ * https://atcoder.jp/contests/typical90/submissions/49664834
  */
 
 void solve() {
     using namespace zawa;
     SetFastIO();
-    const long long INF1{(long long)1e11};
-    const long long INF2{(long long)1e16};
-    int n, w; std::cin >> n >> w;
-    Dinic<long long> mf(n + 2);
-    int s{n}, t{n + 1};
+    int n; std::cin >> n;
+    BurnBury<long long> solver(n);
+    long long w; std::cin >> w;
     for (int i{} ; i < n ; i++) {
-        int a; std::cin >> a;
-        // 入る(燃やす)
-        mf.addEdge(s, i, INF1 - (a - w));
-        // 入らない(埋める)
-        mf.addEdge(i, t, INF1);
+        long long a; std::cin >> a;
+        solver.func1(i, { 0LL, -(a - w) });
     }
+    const long long INF{(long long)1e12};
     for (int i{} ; i < n ; i++) {
         int k; std::cin >> k;
         for (int _{} ; _ < k ; _++) {
-            // 家cに入って(燃やす)家iに入らない(埋める)と無限の損
             int c; std::cin >> c;
             c--;
-            mf.addEdge(i, c, INF2);
+            solver.func2(i, c, { 0LL, INF, 0LL, 0LL });
         }
     }
-    long long ans{mf.flow(s, t)};
-    ans -= INF1 * n;
+    long long ans{solver.build()};
     ans *= -1;
     std::cout << ans << '\n';
 }
