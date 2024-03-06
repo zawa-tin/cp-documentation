5cd296301aff2f2c509ce8765b900d8a55015f06
zawa-tin <contactaizawa@icloud.com>
2024-03-06 20:54:53 +0900

    いけぇええええええええ

--- Test/yukicoder/117.test.cpp
+++ Test/yukicoder/117.test.cpp
@@ -3,10 +3,10 @@
 #include "../../Src/Template/IOSetting.hpp"
 #include "../../Src/Number/BinomalCoefficients.hpp"
 
-// #include <iostream>
-// #include <tuple>
-// #include "../../ac-library/atcoder/modint.hpp"
-// using mint = atcoder::modint1000000007;
+#include <iostream>
+#include <tuple>
+#include "../../ac-library/atcoder/modint.hpp"
+using mint = atcoder::modint1000000007;
 using namespace zawa;
 
 /*
@@ -14,35 +14,35 @@ using namespace zawa;
  * https://yukicoder.me/submissions/957880
  */
 
-// std::tuple<char, int, int> parse() {
-//     std::string s; std::cin >> s;
-//     char c{s[0]};
-//     int i{2};
-//     int p{};
-//     for (; s[i] != ',' ; i++) {
-//         p = (p * 10) + (s[i] - '0');
-//     }
-//     int q{};
-//     for (i++ ; s[i] != ')' ; i++) {
-//         q = (q * 10) + (s[i] - '0');
-//     }
-//     return { c, p, q };
-// }
-// 
-// void solve() {
-//     int t; std::cin >> t;
-//     BinomalCoefficients<mint> comb(1000000);
-//     for (int _{} ; _ < t ; _++) {
-//         auto [c, p, q]{parse()}; 
-//         if (c == 'C') std::cout << comb.C(p, q).val() << '\n';
-//         else if (c == 'P') std::cout << comb.P(p, q).val() << '\n';
-//         else std::cout << comb.H(p, q).val() << '\n';
-//     }
-// }
+std::tuple<char, int, int> parse() {
+    std::string s; std::cin >> s;
+    char c{s[0]};
+    int i{2};
+    int p{};
+    for (; s[i] != ',' ; i++) {
+        p = (p * 10) + (s[i] - '0');
+    }
+    int q{};
+    for (i++ ; s[i] != ')' ; i++) {
+        q = (q * 10) + (s[i] - '0');
+    }
+    return { c, p, q };
+}
+
+void solve() {
+    int t; std::cin >> t;
+    BinomalCoefficients<mint> comb(1000000);
+    for (int _{} ; _ < t ; _++) {
+        auto [c, p, q]{parse()}; 
+        if (c == 'C') std::cout << comb.C(p, q).val() << '\n';
+        else if (c == 'P') std::cout << comb.P(p, q).val() << '\n';
+        else std::cout << comb.H(p, q).val() << '\n';
+    }
+}
 
 int main() {
 #ifdef ONLINE_JUDGE
-    // solve();
+    solve();
 #else
     std::cout << "Hello World" << '\n';
 #endif
