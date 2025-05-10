e6c29e03158cba206edfaa76bb2acbde24d5f5d1
zawa-tin <contactaizawa@icloud.com>
2025-05-10 20:51:44 +0900

    implement SeparatedFamilySet #470

---
+++ Src/Utility/SeparatedFamilySet.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+#include <concepts>
+#include <ranges>
+
+#include "../Template/TypeAlias.hpp"
+
+namespace zawa {
+
+// https://noshi91.hatenablog.com/entry/2024/05/31/012055
+// 向きのついた分割
+std::vector<std::vector<bool>> SeparatedFamilySet(usize U) {
+    const usize d = [&]() {
+        for (usize i = 1 ; ; i++) {
+            usize max = 1;
+            for (usize j = 0 ; j < (i / 2) ; j++) {
+                max *= i - j;
+                max /= j + 1;
+            }
+            if (max >= U) return i;
+        }
+        return U;
+    }();
+    std::vector res(d, std::vector<bool>(U));
+    std::vector<u8> in(d);
+    std::fill(in.rbegin(), in.rbegin() + d / 2, true);
+    for (usize idx = 0 ; idx < U ; idx++) {
+        for (usize i = 0 ; i < d ; i++) if (in[i]) {
+            res[i][idx] = true;
+        }
+        std::ranges::next_permutation(in);
+    }
+    return res;
+}
+
+} // namespace zawa
