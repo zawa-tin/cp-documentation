17157d4c2aa5107833cc49781c211bf0ee493a4d
zawa-tin <contactaizawa@icloud.com>
2025-07-01 18:35:45 +0900

    fix verify link of abc157_f #503

--- Test/AtCoder/abc157_f.test.cpp
+++ Test/AtCoder/abc157_f.test.cpp
@@ -1,7 +1,12 @@
-#define PROBLEM "https://atcoder.jp/contests/abc157/tasks/abc157_f"
-#define ERROR 0.000001
+// #define PROBLEM "https://atcoder.jp/contests/abc157/tasks/abc157_f"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+// #define ERROR 0.000001
 
-#include "../../Src/Template/IOSetting.hpp"
+/*
+ * AtCoder Beginner Contest 157 F - Yakiniku Optimization Problem
+ * https://atcoder.jp/contests/abc157/submissions/67218966
+ */
+
 #include "../../Src/Utility/BinarySearch.hpp"
 #include "../../Src/GeometryR2/Point.hpp"
 #include "../../Src/GeometryR2/Circle.hpp"
@@ -14,11 +19,14 @@ using namespace geometryR2;
 
 #include <algorithm>
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 int main() {
-    SetFastIO();
-    SetPrecision(8);
+#ifdef ATCODER
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios::sync_with_stdio(false);
     int n, k; std::cin >> n >> k;
     std::vector<Point> p(n);
     std::vector<Real> c(n);
@@ -55,5 +63,8 @@ int main() {
     }};
 
     Real ans{BinarySearch(Real{4000 * 100}, Real{}, f, 80)};
-    std::cout << ans << '\n';
+    std::cout << std::fixed << std::setprecision(8) << ans << '\n';
+#else
+    std::cout << "Hello World\n";
+#endif
 }
