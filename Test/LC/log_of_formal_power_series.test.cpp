c5e2e15a4825d5ce05d2a23bf5b24d64d8f0ce0e
zawa-tin <contactaizawa@icloud.com>
2025-08-09 17:28:07 +0900

    implement log inv FPS #527

--- Test/LC/log_of_formal_power_series.test.cpp
+++ Test/LC/log_of_formal_power_series.test.cpp
@@ -1,14 +1,12 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series"
+#define PROBLEM "https://judge.yosupo.jp/problem/log_of_formal_power_series"
 
 #include "../../Src/FPS/FPSNTTFriendly.hpp"
-
-#include <iostream>
-#include <vector>
-
 #include "atcoder/modint"
 using mint = atcoder::modint998244353;
-using namespace std;
 
+#include <iostream>
+
+using namespace std;
 int main() {
     cin.tie(0);
     cout.tie(0);
@@ -17,5 +15,5 @@ int main() {
     cin >> N;
     zawa::FPS<998244353> f(N);
     cin >> f;
-    cout << f.inv() << '\n';
+    cout << f.log() << '\n';
 }
