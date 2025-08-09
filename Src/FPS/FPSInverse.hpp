0ce01bc9a33df1ca2bf8080b3e1c7690dcf36015
zawa-tin <contactaizawa@icloud.com>
2025-08-09 15:27:30 +0900

    バタフライ減らした #527

--- Src/FPS/FPSInverse.hpp
+++ Src/FPS/FPSInverse.hpp
@@ -18,10 +18,26 @@ std::vector<T> FPSInverse(std::vector<T> F, usize n) {
     while (g.size() < n) {
         auto p = g;
         p.resize(g.size() << 1);
-        auto gg = atcoder::convolution(g, g);
-        std::vector<T> f(g.size() << 1);
-        for (usize i = 0 ; i < std::min(f.size(), F.size()) ; i++) f[i] = F[i];
-        auto fgg = atcoder::convolution(f, gg);
+        auto fgg = [&]() {
+            auto res = g;
+            std::vector<T> f(g.size() << 1);
+            for (usize i = 0 ; i < std::min(f.size(), F.size()) ; i++) f[i] = F[i];
+            const usize m = res.size(), k = f.size(), s = (m + m - 1) + k - 1, z = atcoder::internal::bit_ceil(s);
+            res.resize(z);
+            f.resize(z);
+            atcoder::internal::butterfly(res);
+            atcoder::internal::butterfly(f);
+            for (usize i = 0 ; i < z ; i++) res[i] *= res[i] * f[i];
+            atcoder::internal::butterfly_inv(res);
+            res.resize(s);
+            const T iz = T{z}.inv();
+            for (T& v : res) v *= iz;
+            return res;
+        }();
+        // auto gg = atcoder::convolution(g, g);
+        // std::vector<T> f(g.size() << 1);
+        // for (usize i = 0 ; i < std::min(f.size(), F.size()) ; i++) f[i] = F[i];
+        // auto fgg = atcoder::convolution(f, gg);
         fgg.resize(g.size() << 1);
         g.resize(g.size() << 1);
         for (usize i = 0 ; i < ssize(g) ; i++) g[i] = T::raw(2) * p[i] - fgg[i];
