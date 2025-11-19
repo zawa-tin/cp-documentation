fdeab917a5231e7cd377f978556b91ad0c96163e
zawa-tin <contactaizawa@icloud.com>
2025-11-19 23:57:22 +0900

    reverify DSL_2_E

--- Test/AOJ/DSL_2_E.test.cpp
+++ Test/AOJ/DSL_2_E.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_E"
 
-#include "../../Src/DataStructure/SegmentTree/CommutativeDualSegmentTree.hpp"
-#include "../../Src/Algebra/Monoid/AdditionMonoid.hpp"
+#include "../../Src/DataStructure/FenwickTree/DualFenwickTree.hpp"
+#include "../../Src/Algebra/Group/AdditiveGroup.hpp"
 
 #include <iostream>
 
@@ -11,7 +11,7 @@ int main() {
     std::cout.tie(nullptr);
     std::ios::sync_with_stdio(false);
     int n, q; std::cin >> n >> q;
-    CommutativeDualSegmentTree<AdditionMonoid<int>> seg(n);
+    DualFenwickTree<AdditiveGroup<int>> seg(n);
     for (int _{} ; _ < q ; _++) {
         int com; std::cin >> com;
         if (com == 0) {
