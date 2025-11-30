ba04f0a62c5894ed86b589a238922c5282d0e8f1
zawa-tin <contactaizawa@icloud.com>
2025-11-30 18:00:12 +0900

    impl 2dsum's

---
+++ Test/AtCoder/abc434_d.test.cpp
@@ -0,0 +1,44 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+// #define PROBLEM "https://atcoder.jp/contests/abc434/tasks/abc434_d"
+
+#include "../../Src/DataStructure/PrefixSum/PrefixSum2D.hpp"
+#include "../../Src/DataStructure/PrefixSum/Imos2D.hpp"
+#include "../../Src/Algebra/Group/AdditiveGroup.hpp"
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+using namespace std;
+using namespace zawa;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+    const int MAX = 2000;
+    int N;
+    cin >> N;
+    Imos2D<AdditiveGroup<int>> imos(MAX, MAX);
+    vector<tuple<int, int, int, int>> A(N);
+    for (auto& [l, r, d, u] : A) {
+        cin >> l >> r >> d >> u;
+        l--;
+        d--;
+        imos.add(l, d, r, u, 1);
+    }
+    auto a = imos.build();
+    Ruisekiwa2D<AdditiveGroup<int>> sum(MAX, MAX);
+    int base = 0;
+    for (int i = 0 ; i < MAX ; i++)
+        for (int j = 0 ; j < MAX ; j++) {
+            if (a[i][j] == 0)
+                base++;
+            else if (a[i][j] == 1)
+                sum.add(i, j, 1);
+        }
+    auto solver = sum.build();
+    for (auto [l, r, d, u] : A) {
+        int ans = base + solver.product(l, d, r, u);
+        cout << ans << '\n';
+    }
+}
