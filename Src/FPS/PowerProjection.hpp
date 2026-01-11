0eca0f9992694b5c1b963cf2ee2d5858d42f46e8
zawa-tin <contactaizawa@icloud.com>
2026-01-12 03:09:14 +0900

    too slow! powerproj

---
+++ Src/FPS/PowerProjection.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "FPS.hpp"
+
+#include <algorithm>
+#include <cassert>
+#include <bit>
+#include <vector>
+#include <ranges>
+
+namespace zawa {
+
+// i = 0, 1, 2, \dots, M - 1
+// res[i] = \sum_{j} W([x^j]F(x)^i)
+template <concepts::IndexedFPS FPS, class Conv = FPSMult>
+requires concepts::Convolution<FPS, Conv>
+std::vector<typename FPS::value_type> PowerProjection(usize m, FPS W, FPS F, Conv convolution = {}) {
+    using Element = typename FPS::value_type;
+    assert(W.size());
+    if (!m)
+        return {};
+    F.resize(W.size());
+    if (F[0] != Element{0}) {
+        std::vector<Element> fact(m, 1), invFact(m);
+        for (usize i = 1 ; i < m ; i++)
+            fact[i] = fact[i - 1] * Element{i};
+        invFact[m - 1] = Element{1} / fact[m - 1];
+        for (usize i = m ; --i ; )
+            invFact[i - 1] = invFact[i] * Element{i};
+        const Element c = F[0];
+        F[0] = 0;
+        FPS cur{PowerProjection(m, W, F, convolution)};
+        FPS op(m);
+        Element pw = 1;
+        for (usize i = 0 ; i < m ; i++, pw *= c) {
+            op[i] = pw * invFact[i];
+            cur[i] *= invFact[i];
+        }
+        auto res = convolution(cur, op);
+        res.resize(m);
+        for (usize i = 0 ; i < m ; i++)
+            res[i] *= fact[i];
+        return res;
+    }
+    usize nx = std::bit_ceil(W.size()), ny = 4;
+    W.resize(nx);
+    F.resize(nx);
+    std::ranges::reverse(W);
+    auto get = [&](FPS& f, usize x, usize y) -> Element& {
+        assert(x < nx);
+        assert(y < ny);
+        return f[x * ny + y];
+    };
+    // bostan mori [x^{n-1}](g(x)/(1-yf(x)))
+    FPS P(nx * ny), Q(nx * ny);
+    get(Q, 0, 0) = 1;
+    for (usize x = 0 ; x < nx ; x++) {
+        get(P, x, 0) = W[x];
+        get(Q, x, 1) = -F[x];
+    }
+    while (nx > 1) {
+        FPS R(nx * ny);
+        for (usize x = 0 ; x < nx ; x++)
+            for (usize y = 0 ; y < (ny >> 1) ; y++)
+                get(R, x, y) = get(Q, x, y) * Element{(x % 2 ? -1 : 1)};
+        auto PR = convolution(P, R), QR = convolution(Q, R);
+        std::ranges::fill(P, Element{0});
+        std::ranges::fill(Q, Element{0});
+        for (usize x = 1 ; x < nx ; x += 2)
+            for (usize y = 0 ; y < ny ; y++)
+                P[(x >> 1) * (ny << 1) + y] = get(PR, x, y);
+        for (usize x = 0 ; x < nx ; x += 2)
+            for (usize y = 0 ; y < ny ; y++) 
+                Q[(x >> 1) * (ny << 1) + y] = get(QR, x, y);
+        nx >>= 1;
+        ny <<= 1;
+    }
+    P.resize(m);
+    return P;
+}
+
+} // namespace zawa
---
+++ Test/My/FPS/PowerProjection.test.cpp
@@ -0,0 +1,80 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "../../../Src/FPS/FPSNTTFriendly.hpp"
+#include "../../../Src/FPS/PowerProjection.hpp"
+#include "atcoder/modint"
+using namespace zawa;
+using mint = atcoder::modint998244353;
+using fps = FPSNTTFriendly<mint::mod()>;
+
+#include <iostream>
+#include <iterator>
+#include <random>
+using namespace std;
+mt19937 mt{random_device{}()};
+vector<mint> naive(int M, const fps& W, const fps& F) {
+    vector<mint> res(M);
+    fps cur{1};
+    for (int i = 0 ; i < M ; i++) {
+        cur.resize(W.size());
+        for (int j = 0 ; j < ssize(W) ; j++)
+            res[i] += W[j] * cur[j];
+        cur *= F;
+    }
+    return res;
+}
+void test(int MAXN, int MAXM, int MAXVAL, bool verify = true) {
+    int N = mt() % MAXN + 1;
+    int M = mt() % MAXM + 1;
+    fps W(N), F(mt() % MAXN + 1);
+    for (int i = 0 ; i < N ; i++)
+        W[i] = mt() % MAXVAL;
+    for (int i = 0 ; i < ssize(F) ; i++)
+        F[i] = mt() % MAXVAL;
+    auto ans = PowerProjection(M, W, F);
+    if (verify) {
+        auto nv = naive(M, W, F);
+        if (ans != nv) {
+            cerr << W << endl;
+            cerr << F << endl;
+            cerr << "ans: ";
+            for (auto i : nv)
+                cerr << i.val() << ' ';
+            cerr << endl;
+            cerr << "you: ";
+            for (auto i : ans)
+                cerr << i.val() << ' ';
+            cerr << endl;
+            exit(0);
+        }
+    }
+}
+int main() {
+#ifdef DEBUG
+    fps W{4, 0, 4};
+    fps F{0, 3};
+    int M = 4;
+    for (auto i : PowerProjection(M, W, F))
+        cout << i.val() << ' ';
+    cout << endl;
+    for (auto i : naive(M, W, F))
+        cout << i.val() << ' ';
+    cout << endl;
+#else
+    //for (int i = (1 << 10) ; i-- ; )
+    //    test(5, 5, 5);
+    //cerr << "tiny end" << endl;
+    //for (int i = (1 << 10) ; i-- ; )
+    //    test(50, 50, mint::mod());
+    //cerr << "small end" << endl;
+    //for (int i = 0 ; i < (1 << 3) ; i++)
+    //    test(3000, 3000, mint::mod());
+    //cerr << "med end" << endl;
+    for (int i = 0 ; i < (1 << 2) ; i++)
+        test(200000, 200000, mint::mod(), false);
+    cerr << "large end" << endl;
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << '\n';
+#endif
+}
