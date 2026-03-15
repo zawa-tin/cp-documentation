aa5058ce2f39eaff61be7ef309eab663708ca3c4
zawa-tin <contactaizawa@icloud.com>
2026-03-15 23:24:20 +0900

    modify signature

--- Src/Sequence/MoRangeQuery.hpp
+++ Src/Sequence/MoRangeQuery.hpp
@@ -8,7 +8,7 @@
 namespace zawa {
 
 template <class T, class AddL, class AddR, class DelL, class DelR, class Eval>
-std::vector<typename std::invoke_result_t<Eval, usize>> Mo(std::vector<std::pair<T,T>> qs, AddL addL, AddR addR, DelL delL, DelR delR, Eval eval, bool reset = false) {
+std::vector<typename std::invoke_result_t<Eval, usize>> Mo(const std::vector<std::pair<T,T>>& qs, AddL addL, AddR addR, DelL delL, DelR delR, Eval eval, bool reset = false) {
     auto ord = Mo(qs);
     std::vector<typename std::invoke_result_t<Eval, usize>> res(qs.size());
     T L = 0, R = 0;
