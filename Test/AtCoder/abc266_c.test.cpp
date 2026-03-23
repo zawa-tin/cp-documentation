3709c8905ae95dec5a80943f567d9e327a94c53c
zawa-tin <contactaizawa@icloud.com>
2026-03-24 01:33:58 +0900

    verifylink修正

--- Test/AtCoder/abc266_c.test.cpp
+++ Test/AtCoder/abc266_c.test.cpp
@@ -1,12 +1,17 @@
-#define PROBLEM "https://atcoder.jp/contests/abc266/tasks/abc266_c"
+// #define PROBLEM "https://atcoder.jp/contests/abc266/tasks/abc266_c"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
 
 #include "../../Src/GeometryZ2/Polygon.hpp"
 
 #include <iostream>
 
 int main() {
+#ifdef ATCODER
     using namespace zawa::geometryZ2;
     Polygon p(4);
     std::cin >> p;
     std::cout << (p.isConvex() ? "Yes\n" : "No\n");
+#else
+    std::cout << "Hello World\n";
+#endif
 }
